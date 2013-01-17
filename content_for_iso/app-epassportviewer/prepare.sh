764d2d5e687d9d51821d0d44e9981a192f0ba737
Philippe Teuwen <yobibe@gmail.com>
2013-01-17 01:34:10 +0100

    ePassportViewer: upgrade to 2.0.14

--- content_for_iso/app-epassportviewer/prepare.sh
+++ content_for_iso/app-epassportviewer/prepare.sh
@@ -1,60 +1,85 @@
 #!/bin/bash
 
-# epassportviewer
-svn export http://epassportviewer.googlecode.com/svn/trunk/epassportviewer/src/epassportviewer config/includes.chroot/usr/local/lib/epassportviewer/epassportviewer
-mv config/includes.chroot/usr/local/lib/epassportviewer/epassportviewer/ePassportViewer.py config/includes.chroot/usr/local/lib/epassportviewer/
-# pypassport
-svn export http://pypassport.googlecode.com/svn/trunk/pypassport/src/pypassport config/includes.chroot/usr/local/lib/epassportviewer/pypassport
-# fix epassportviewer temp files location
-cd config/includes.chroot/usr/local/lib/
+# sudo apt-get install python-dev python-setuptools
+
+# epassportviewer 2.0.14
+VERSION=2.0.14
+wget -nc -O ePassportViewer-$VERSION.tar.gz "http://ge.tt/api/1/files/6Exz0YO/2/blob?download"
+mkdir -p config/includes.chroot/usr/local/lib
+tar xzf ePassportViewer-$VERSION.tar.gz --strip-components=1 -C config/includes.chroot/usr/local/lib ePassportViewer-2.0.14/ePassportViewer-2.0
+rm -rf config/includes.chroot/usr/local/lib/ePassportViewer-2.0/geojasper
+# patch upper()
+cd config/includes.chroot/usr/local/lib/ePassportViewer-2.0
 patch -p0 <<EOF
-diff -Naur epassportviewer/epassportviewer/const.py epassportviewer/epassportviewer/const.py
---- epassportviewer/epassportviewer/const.py	2010-03-25 01:36:37.000000000 +0100
-+++ epassportviewer/epassportviewer/const.py	2011-05-28 23:45:46.000000000 +0200
-@@ -22,10 +22,10 @@
+--- epassportviewer/util/forge.py	2012-08-01 11:13:39.000000000 +0200
++++ epassportviewer/util/forge.py	2013-01-16 23:19:30.457200482 +0100
+@@ -105,7 +105,7 @@
+         # Full name
+         if middle_name:
+             middle_name = middle_name.replace(' ', '<')
+-            full_name = surname.uppercase + "<<" + firstname.uppercase + middle_name.uppercase
++            full_name = surname.upper() + "<<" + firstname.upper() + middle_name.upper()
+             dgc.addDataObject("5F02", full_name)
+          
+         # Place of birth
+EOF
+# fix epassportviewer temp files location
+patch -p0 <<EOF
+--- epassportviewer/const.py	2012-08-22 10:31:44.000000000 +0200
++++ epassportviewer/const.py	2013-01-17 01:23:15.679957082 +0100
+@@ -20,12 +20,12 @@
+ 
+ # Options
  DEBUG   = True
- STDERR  = 'error.log'
- CONFIG  = 'config.ini'
+-STDERR  = 'error.log'
+-CONFIG  = 'config.ini'
 -HISTORY = 'history'
-+HISTORY = '/tmp/epp-history'
++STDERR  = '/tmp/ePV-error.log'
++CONFIG  = '/tmp/ePV-config.ini'
++HISTORY = '/tmp/ePV-history'
  MAX_HISTORY = 10
  TITLE   = 'ePassport Viewer'
 -LOG     = 'system.log'
-+LOG     = '/tmp/epp-system.log'
++LOG     = '/tmp/ePV-system.log'
  
- VERSION = '1.0'
+ VERSION = '2.0'
  WEBSITE = "http://code.google.com/p/epassportviewer/"
-diff -Naur epassportviewer/ePassportViewer.py epassportviewer/ePassportViewer.py
---- epassportviewer/ePassportViewer.py	2010-03-25 01:36:34.000000000 +0100
-+++ epassportviewer/ePassportViewer.py	2011-05-28 23:46:20.000000000 +0200
-@@ -33,7 +33,7 @@
+--- ePassportViewer.py	2012-08-09 16:56:00.000000000 +0200
++++ ePassportViewer.py	2013-01-17 01:22:42.559357140 +0100
+@@ -35,7 +35,7 @@
  class dummyStream(object):
      ''' dummyStream behaves like a stream but does nothing. '''
      def __init__(self): 
 -        self.f = open('out.txt', 'w')
-+        self.f = open('/tmp/epp-out.txt', 'w')
++        self.f = open('/tmp/ePV-out.txt', 'w')
      def write(self,data): 
          self.f.write(data)
      def read(self,data): pass
 EOF
 cd -
-# fix pypassport DES3 IV
-cd config/includes.chroot/usr/local/lib/epassportviewer
-patch -p3 < ../../../../../../pypassport-des3-iv.patch
-cd -
+tar xzf ePassportViewer-$VERSION.tar.gz --strip-components=2 -C config/includes.chroot/usr/local/lib/ePassportViewer-2.0 ePassportViewer-2.0.14/pypassport-2.0/pypassport
+
+# cleaning
+find config/includes.chroot/usr/local/lib/ePassportViewer-2.0 -name "*.pyc" -or -name "*~" -exec rm {} \;
+
 # epassportviewer helper
 mkdir -p config/includes.chroot/usr/local/bin
 cat > config/includes.chroot/usr/local/bin/ePassportViewer << EOF
 #!/bin/bash
-cd /usr/local/lib/epassportviewer
-python ePassportViewer.py
+python /usr/local/lib/ePassportViewer-2.0/ePassportViewer.py
 EOF
-# geojasper
-wget -O - http://www.dimin.net/software/geojasper/geojasper_linux32.tgz|tar xz --strip-components=1 -C config/includes.chroot/usr/local/bin geojasper/geojasper
-chmod 755 config/includes.chroot/usr/local/bin/*
+chmod 755 config/includes.chroot/usr/local/bin/ePassportViewer
+
 # pypassport doc
-mkdir -p config/includes.chroot/home/user/Desktop/docs/applications/epassportviewer
-wget -nc http://pypassport.googlecode.com/files/pypassport-1.0-doc.zip
+#mkdir -p config/includes.chroot/home/user/Desktop/docs/applications/epassportviewer
+#wget -nc http://pypassport.googlecode.com/files/pypassport-1.0-doc.zip
 # epassportviewer doc
-unzip -d config/includes.chroot/home/user/Desktop/docs/applications/epassportviewer pypassport-1.0-doc.zip
-wget -nc -P config/includes.chroot/home/user/Desktop/docs/applications/epassportviewer http://epassportviewer.googlecode.com/files/epassportviewer-0.2c.manual.pdf
+#unzip -d config/includes.chroot/home/user/Desktop/docs/applications/epassportviewer pypassport-1.0-doc.zip
+#wget -nc -P config/includes.chroot/home/user/Desktop/docs/applications/epassportviewer http://epassportviewer.googlecode.com/files/epassportviewer-0.2c.manual.pdf
+
+# geojasper
+wget -nc http://www.dimin.net/software/geojasper/geojasper_linux32.tgz
+mkdir -p config/includes.chroot/usr/local/bin
+tar xzf geojasper_linux32.tgz --strip-components=1 -C config/includes.chroot/usr/local/bin geojasper/geojasper
+chmod 755 config/includes.chroot/usr/local/bin/geojasper
+
