fe04355b596f15cc1c74bdac6972d282f55d5d23
Philippe Teuwen <yobibe@gmail.com>
2013-01-17 23:05:53 +0100

    Add PCSC docs

---
+++ content_for_iso/doc-pcsc/add.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p ../@config/includes.chroot/home/user/Desktop/docs/technology/PCSC/
+unzip -d ../@config/includes.chroot/home/user/Desktop/docs/technology/PCSC/ pcsc1-10_v*.zip
+cp *.pdf ../@config/includes.chroot/home/user/Desktop/docs/technology/PCSC/
---
+++ content_for_iso/doc-pcsc/prepare.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+REVISION=2.01.13
+
+wget -nc http://www.pcscworkgroup.com/specifications/files/pcsc1-10_v$REVISION.zip
+wget -nc http://www.pcscworkgroup.com/library/files/PCSC2_0WhitePaper.pdf
+wget -nc http://www.stismartcard.com/download/pcsc.pdf
