4e6c990744c963c1fb24a948324d86f5a0a7dc63
Philippe Teuwen <yobibe@gmail.com>
2013-01-19 23:36:23 +0100

    ePV: more patches

--- content_for_iso/app-epassportviewer/prepare.sh
+++ content_for_iso/app-epassportviewer/prepare.sh
@@ -9,71 +9,12 @@ wget -nc -O download/ePassportViewer-$VERSION.tar.gz "http://ge.tt/api/1/files/6
 mkdir -p config/includes.chroot/usr/local/lib
 tar xzf download/ePassportViewer-$VERSION.tar.gz --strip-components=1 -C config/includes.chroot/usr/local/lib ePassportViewer-2.0.14/ePassportViewer-2.0
 rm -rf config/includes.chroot/usr/local/lib/ePassportViewer-2.0/geojasper
-# patch upper()
-cd config/includes.chroot/usr/local/lib/ePassportViewer-2.0
-patch -p0 <<EOF
---- epassportviewer/util/forge.py	2012-08-01 11:13:39.000000000 +0200
-+++ epassportviewer/util/forge.py	2013-01-16 23:19:30.457200482 +0100
-@@ -105,7 +105,7 @@
-         # Full name
-         if middle_name:
-             middle_name = middle_name.replace(' ', '<')
--            full_name = surname.uppercase + "<<" + firstname.uppercase + middle_name.uppercase
-+            full_name = surname.upper() + "<<" + firstname.upper() + middle_name.upper()
-             dgc.addDataObject("5F02", full_name)
-          
-         # Place of birth
-EOF
-# fix epassportviewer temp files location
-patch -p0 <<EOF
---- epassportviewer/const.py	2012-08-22 10:31:44.000000000 +0200
-+++ epassportviewer/const.py	2013-01-18 11:54:05.221628307 +0100
-@@ -20,12 +20,12 @@
- 
- # Options
- DEBUG   = True
--STDERR  = 'error.log'
--CONFIG  = 'config.ini'
--HISTORY = 'history'
-+STDERR  = os.path.expanduser('~/ePV-error.log')
-+CONFIG  = os.path.expanduser('~/.ePV-config.ini')
-+HISTORY = os.path.expanduser('~/.ePV-history')
- MAX_HISTORY = 10
- TITLE   = 'ePassport Viewer'
--LOG     = 'system.log'
-+LOG     = os.path.expanduser('~/ePV-system.log')
- 
- VERSION = '2.0'
- WEBSITE = "http://code.google.com/p/epassportviewer/"
---- ePassportViewer.py	2012-08-09 16:56:00.000000000 +0200
-+++ ePassportViewer.py	2013-01-18 11:52:52.424006937 +0100
-@@ -35,7 +35,7 @@
- class dummyStream(object):
-     ''' dummyStream behaves like a stream but does nothing. '''
-     def __init__(self): 
--        self.f = open('out.txt', 'w')
-+        self.f = open(os.path.expanduser('~/ePV-out.txt'), 'w')
-     def write(self,data): 
-         self.f.write(data)
-     def read(self,data): pass
-EOF
-# do not rehash CSCA
-patch -p0 <<EOF
---- epassportviewer/dialog.py	2012-09-24 15:07:45.000000000 +0200
-+++ epassportviewer/dialog.py	2013-01-18 02:01:59.784412116 +0100
-@@ -769,7 +769,7 @@
-                 
-                 try:
-                     if configManager().getOption('Options','certificate'):
--                        self.doc.setCSCADirectory(configManager().getOption('Options','certificate'), True)
-+                        self.doc.setCSCADirectory(configManager().getOption('Options','certificate'), False)
-                     certif = self.doc.doVerifySODCertificate()
-                     if not configManager().getOption('Options','certificate'):
-                         certif = "CA_NOT_SET"
-EOF
-cd -
 tar xzf certificates.tgz -C config/includes.chroot/usr/local/lib/ePassportViewer-2.0/epassportviewer/ressources/
 tar xzf download/ePassportViewer-$VERSION.tar.gz --strip-components=2 -C config/includes.chroot/usr/local/lib/ePassportViewer-2.0 ePassportViewer-2.0.14/pypassport-2.0/pypassport
+# patches
+cd config/includes.chroot/usr/local/lib/ePassportViewer-2.0
+patch -p2 < ../../../../../../ePV.diff
+cd -
 
 # cleaning & fixing rights
 find config/includes.chroot/usr/local/lib/ePassportViewer-2.0 -name "*.pyc" -or -name "*~" -exec rm {} \;
