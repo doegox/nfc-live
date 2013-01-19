94f65fc23abb934c21eb68191740916985e66083
Philippe Teuwen <yobibe@gmail.com>
2013-01-20 00:26:29 +0100

    ifdnfc: provide helper scripts to swap between configs

--- content_for_iso/driver-ifdnfc.devel/config/includes.chroot/root/live-ifdnfc.sh
+++ content_for_iso/driver-ifdnfc.devel/config/includes.chroot/root/live-ifdnfc.sh
@@ -28,3 +28,36 @@ EOF
 chmod 755 /tmp/TRANSFER/driver-ifdnfc.generated/add.sh
 # Avoid conflict with SCL3711 driver:
 sed -i -e '/0x04E6\|0x5591\|SCL3711/s/\(.*\)/<!-- \1 -->/' /tmp/TRANSFER/driver-ifdnfc.generated/config/includes.chroot/usr/lib/pcsc/drivers/ifdnfc.bundle/Contents/Info.plist
+# Provide helper scripts
+mkdir -p /tmp/TRANSFER/driver-ifdnfc.generated/config/includes.chroot/usr/local/bin/
+cat > /tmp/TRANSFER/driver-ifdnfc.generated/config/includes.chroot/usr/local/bin/scl3711-pcsc_proprio << EOF
+#!/bin/bash
+
+sudo /etc/init.d/pcscd stop
+# comment out SCL3711 from ifdnfc
+sudo sed -i -e '/^[[:space:]]*<string>\(0x04E6\|0x5591\|SCM\)/s/\(.*\)/<!-- \1 -->/' /usr/lib/pcsc/drivers/ifdnfc.bundle/Contents/Info.plist
+# activate SCL3711 proprietary driver
+sudo mv /usr/lib/pcsc/drivers/SCx371x.bundle/Contents/Info.plist.disabled /usr/lib/pcsc/drivers/SCx371x.bundle/Contents/Info.plist 2>/dev/null
+sudo /etc/init.d/pcscd start
+EOF
+cat > /tmp/TRANSFER/driver-ifdnfc.generated/config/includes.chroot/usr/local/bin/scl3711-pcsc_ifdnfc << EOF
+#!/bin/bash
+
+sudo /etc/init.d/pcscd stop
+# add SCL3711 to ifdnfc
+sudo sed -i -e '/^<!--[[:space:]]*<string>\(0x04E6\|0x5591\|SCM\)/s/<!-- //;s/ -->//' /usr/lib/pcsc/drivers/ifdnfc.bundle/Contents/Info.plist
+# deactivate SCL3711 proprietary driver
+sudo mv /usr/lib/pcsc/drivers/SCx371x.bundle/Contents/Info.plist /usr/lib/pcsc/drivers/SCx371x.bundle/Contents/Info.plist.disabled 2>/dev/null
+sudo /etc/init.d/pcscd start
+EOF
+cat > /tmp/TRANSFER/driver-ifdnfc.generated/config/includes.chroot/usr/local/bin/scl3711-libnfc << EOF
+#!/bin/bash
+
+sudo /etc/init.d/pcscd stop
+# comment out SCL3711 from ifdnfc
+sudo sed -i -e '/^[[:space:]]*<string>\(0x04E6\|0x5591\|SCM\)/s/\(.*\)/<!-- \1 -->/' /usr/lib/pcsc/drivers/ifdnfc.bundle/Contents/Info.plist
+# deactivate SCL3711 proprietary driver
+sudo mv /usr/lib/pcsc/drivers/SCx371x.bundle/Contents/Info.plist /usr/lib/pcsc/drivers/SCx371x.bundle/Contents/Info.plist.disabled 2>/dev/null
+sudo /etc/init.d/pcscd start
+EOF
+chmod 755 /tmp/TRANSFER/driver-ifdnfc.generated/config/includes.chroot/usr/local/bin/*
