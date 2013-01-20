5642af5aabc3d53904a63abd81f80d5878582093
Philippe Teuwen <yobibe@gmail.com>
2013-01-20 02:13:36 +0100

    smartcard_list: wget -N -O not as expected, use wget -N -P

--- content_for_iso/app-pcscd/prepare.sh
+++ content_for_iso/app-pcscd/prepare.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 mkdir -p config/includes.chroot/root/ config/includes.chroot/etc/skel/
-wget -N -O config/includes.chroot/root/.smartcard_list.txt http://ludovic.rousseau.free.fr/softwares/pcsc-tools/smartcard_list.txt
-cp config/includes.chroot/root/.smartcard_list.txt config/includes.chroot/etc/skel/.smartcard_list.txt
+wget -N -P download http://ludovic.rousseau.free.fr/softwares/pcsc-tools/smartcard_list.txt
+cp download/smartcard_list.txt config/includes.chroot/root/.smartcard_list.txt
+cp download/smartcard_list.txt config/includes.chroot/etc/skel/.smartcard_list.txt
