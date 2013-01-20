4d13a9bdc18b68c9c0400ec7722ad3c369f31e0e
Philippe Teuwen <yobibe@gmail.com>
2013-01-21 00:45:27 +0100

    pm3: change fw & doc location

--- content_for_iso/app-proxmark3fw/add.sh
+++ content_for_iso/app-proxmark3fw/add.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -e config/includes.chroot/root/proxmark3/firmware_* ]; then
+if [ ! -e config/includes.chroot/usr/local/share/proxmark3/firmware_* ]; then
   echo "Error: missing Proxmark firmware, please read howto.txt"
   exit 1
 fi
