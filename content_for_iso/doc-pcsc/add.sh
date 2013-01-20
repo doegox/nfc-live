e6ecc9d90af85798793cf4405117b1b45ab9975a
Philippe Teuwen <yobibe@gmail.com>
2013-01-20 02:47:17 +0100

    pcsc: force unzip

--- content_for_iso/doc-pcsc/add.sh
+++ content_for_iso/doc-pcsc/add.sh
@@ -5,5 +5,5 @@ if [ ! -e download ]; then
     exit 1
 fi
 mkdir -p ../@config/includes.binary/nfc-doc/technology/PCSC/
-unzip -d ../@config/includes.binary/nfc-doc/technology/PCSC/ download/pcsc1-10_v*.zip
+unzip -o -d ../@config/includes.binary/nfc-doc/technology/PCSC/ download/pcsc1-10_v*.zip
 cp download/*.pdf ../@config/includes.binary/nfc-doc/technology/PCSC/
