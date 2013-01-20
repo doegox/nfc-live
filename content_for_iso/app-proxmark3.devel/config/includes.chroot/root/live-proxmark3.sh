3fc909c9a6eeab72765c825d8cbef4ce7381eea9
Philippe Teuwen <yobibe@gmail.com>
2013-01-21 00:46:23 +0100

    pm3: give rights to user & move bins to /usr/local/bin

--- content_for_iso/app-proxmark3.devel/config/includes.chroot/root/live-proxmark3.sh
+++ content_for_iso/app-proxmark3.devel/config/includes.chroot/root/live-proxmark3.sh
@@ -5,19 +5,19 @@ REVISION=649
 svn export -r $REVISION http://proxmark3.googlecode.com/svn/trunk/ proxmark3-dev
 cd proxmark3-dev
 make client
-mkdir -p /tmp/TRANSFER/app-proxmark3.generated/config/includes.chroot/root/proxmark3/tools
-cp -a traces /tmp/TRANSFER/app-proxmark3.generated/config/includes.chroot/root/proxmark3
-cp -a tools/findbits.py tools/rfidtest.pl tools/xorcheck.py /tmp/TRANSFER/app-proxmark3.generated/config/includes.chroot/root/proxmark3/tools
+mkdir -p /tmp/TRANSFER/app-proxmark3.generated/config/includes.chroot/usr/local/bin
+mkdir -p /tmp/TRANSFER/app-proxmark3.generated/config/includes.chroot/usr/local/share/proxmark3
+cp -a traces /tmp/TRANSFER/app-proxmark3.generated/config/includes.chroot/usr/local/share/proxmark3
+cp -a tools/findbits.py tools/rfidtest.pl tools/xorcheck.py /tmp/TRANSFER/app-proxmark3.generated/config/includes.chroot/usr/local/bin
 cd tools/mfkey
 make
-cp -a mfkey /tmp/TRANSFER/app-proxmark3.generated/config/includes.chroot/root/proxmark3/tools
+cp -a mfkey /tmp/TRANSFER/app-proxmark3.generated/config/includes.chroot/usr/local/bin
 cd -
 cd tools/nonce2key
 make
-cp -a nonce2key /tmp/TRANSFER/app-proxmark3.generated/config/includes.chroot/root/proxmark3/tools
+cp -a nonce2key /tmp/TRANSFER/app-proxmark3.generated/config/includes.chroot/usr/local/bin
 cd -
-cp -a LICENSE.txt README.txt /tmp/TRANSFER/app-proxmark3.generated/config/includes.chroot/root/proxmark3
-cp -a client/cli client/flasher client/proxmark3 client/snooper client/unbind-proxmark /tmp/TRANSFER/app-proxmark3.generated/config/includes.chroot/root/proxmark3
+cp -a client/cli client/flasher client/proxmark3 client/snooper client/unbind-proxmark /tmp/TRANSFER/app-proxmark3.generated/config/includes.chroot/usr/local/bin
 mkdir -p /tmp/TRANSFER/app-proxmark3.generated/config/includes.binary/nfc-doc/applications/proxmark3
 cp doc/*pdf /tmp/TRANSFER/app-proxmark3.generated/config/includes.binary/nfc-doc/applications/proxmark3
 mkdir -p /tmp/TRANSFER/app-proxmark3.generated/config/package-lists
